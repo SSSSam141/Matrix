1c7d6e41d466f0769c3fc913a9a78b542efdac80
XiShao <11712611@mail.sustech.edu.cn>
2020-06-09 10:31:42 +0800

    delete

--- main.cpp
+++ main.cpp
@@ -8,9 +8,7 @@ int main()
     cout << "OpenCV Version: " << CV_VERSION << endl;
 
    // namedWindow("游戏原画");
-    cout<<img.size<<endl;
-    imshow("游戏原画", img);
-    waitKey(0);
+
     return 0;
 }
 
